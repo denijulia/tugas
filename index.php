4f6fc0cccf0838a6659838c1c2e11f14c4724dd3
denijulia <79452365+denijulia@users.noreply.github.com>
2021-02-25 23:34:15 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>Tugas Edi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+	<meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+	<link rel="canonical" href="http://informatika.undiksha.ac.id/">
+	<meta property="og:locale" content="en_US">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Manajemen Informatika Universitas Pendidikan Ganesha">
+	<meta property="og:description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+	<meta property="og:url" content="http://informatika.undiksha.ac.id/">
+	<meta property="og:site_name" content="Manajemen Informatika Universitas Pendidikan Ganesha">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+    <div class="jumbotron text-center">
+  <h1>Sistem Informasi Zodiak</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+        <div class ="sidebar">
+            <h3>Column 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+    </body>
+</html>
