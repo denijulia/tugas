5bb7a6ec66eddd68a9ade3c9748c4a3761eb283c
denijulia <79452365+denijulia@users.noreply.github.com>
2021-03-07 13:26:53 +0800

    Add files via upload

---
+++ deni/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>Tugas Deni</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+	<meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+	<link rel="canonical" href="http://informatika.undiksha.ac.id/">
+	<meta property="og:locale" content="en_US">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Manajemen Informatika Universitas Pendidikan Ganesha">
+	<meta property="og:description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+	<meta property="og:url" content="http://informatika.undiksha.ac.id/">
+	<meta property="og:site_name" content="Manajemen Informatika Universitas Pendidikan Ganesha">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <section class="header">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#">Sistem informasi penjualan kopi sepang</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto text-right">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="media/form.html">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
+
+    </section>
+
+        </div>
+    <div class="jumbotron text-center">
+  <h1>Sistem Informasi Penjualan Kopi Sepang</h1>
+  <p>Ni Ketut Deni Julia Marlina (1915101044)</p> 
+  <img src="media\gambar kopi.jpg">
+</div>  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+        <div class ="sidebar">
+            <h3>Arabika</h3>
+            <p>mengeluarkan aroma segar seperti buah blueberry</p>
+        </div>
+    </div>
+    <div class="col-sm-4">
+      <h3>Robusta</h3>
+      <p>Memiliki Aroma yang netral dan kurang bervariasi</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Macchiato</h3>        
+      <p>merupakan espresso yang dicampur susu foam</p>
+    </div>
+  </div>
+</div>
+    </body>
+</html>
